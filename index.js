1fdcee864594a83ac5a7118da5bec68b57449301
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-08-16 09:54:18 -0400

    Build out initial server implementation in 'index.js'.

---
+++ index.js
@@ -0,0 +1,29 @@
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+// Node
+const http = require( 'http' );
+
+// --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+const PORT = 8080;
+
+const unsupportedRoutes = [ '/favicon.ico', '/robots.txt' ]; /// TEMP
+
+// --------------------------------------------------
+// DECLARE FUNCTIONS
+// --------------------------------------------------
+
+// --------------------------------------------------
+// INIT
+// --------------------------------------------------
+http.createServer( ( req, res ) => {
+	if ( unsupportedRoutes.includes( req.url ) ) {
+		res.end();
+	} else {
+		res.end( 'Hello, world!' );
+	}
+} ).listen( PORT, () => {
+	console.log( `LISTENING ON PORT: ${PORT}` );
+} )
