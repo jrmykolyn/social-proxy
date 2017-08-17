211dfadfcda6301d83db11ec580b5f2588b9895f
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-08-16 21:30:58 -0400

    Complete first pass of application functionality: fetch data from Instagram API.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 // Node
 const http = require( 'http' );
 
+// Vendor
+const curl = require( 'curl' );
+const pg = require( 'pg' );
+const Promise = require( 'bluebird' );
+
 // --------------------------------------------------
 // DECLARE VARS
 // --------------------------------------------------
@@ -11,19 +16,114 @@ const PORT = process.env.PORT || 8080;
 
 const unsupportedRoutes = [ '/favicon.ico', '/robots.txt' ]; /// TEMP
 
+const Client = pg.Client;
+
+var db;
+
 // --------------------------------------------------
 // DECLARE FUNCTIONS
 // --------------------------------------------------
+function parseRoute( route ) {
+	return new Promise( ( resolve, reject ) => {
+		let routeBits = route.split( '/' ).filter( ( bit ) => { return bit !== '' } );
 
-// --------------------------------------------------
-// INIT
-// --------------------------------------------------
-http.createServer( ( req, res ) => {
-	if ( unsupportedRoutes.includes( req.url ) ) {
-		res.end();
-	} else {
-		res.end( 'Hello, world!' );
+		resolve( routeBits );
+	} );
+}
+
+function dbConnect() {
+	return new Promise( ( resolve, reject ) => {
+		db = new Client( {
+			database: 'social_proxy',
+		} );
+
+		resolve( db );
+	} );
+}
+
+function getAccessToken( provider, username ) {
+	return new Promise( ( resolve, reject ) => {
+		db.connect();
+
+		db.query( `SELECT * FROM access_tokens WHERE username = '${username}' AND provider = '${provider}'`, ( err, result ) => {
+			if ( err ) {
+				console.log( 'ENCOUNTERED ERROR' ); /// TEMP
+				console.log( err );
+			}
+
+			db.end();
+
+			if ( !err && result && result.rows && result.rows.length ) {
+				resolve( result.rows[ 0 ] );
+			} else {
+				reject( err || `No matches found for the following provider and username: ${provider}; ${username}` );
+			}
+		} );
+	} );
+}
+
+function fetchInstagramData( accessToken ) {
+	return new Promise( ( resolve, reject ) => {
+		if ( !accessToken ) {
+			reject( 'Missing or invalid access token.' );
+		}
+
+		curl.get(
+			`https://api.instagram.com/v1/users/self/media/recent?access_token=${accessToken}`,
+			{
+				CURLOPT_RETURNTRANSFER: 1,
+				CURLOPT_FOLLOWLOCATION: true,
+			},
+			function( err, response, body ) {
+				if ( err ) {
+					reject( err );
+				}
+
+				resolve( body );
+			} );
+		} );
 	}
-} ).listen( PORT, () => {
-	console.log( `LISTENING ON PORT: ${PORT}` );
-} )
+
+	// --------------------------------------------------
+	// INIT
+	// --------------------------------------------------
+	http.createServer( ( req, res ) => {
+		// ...
+		res.setHeader( 'Access-Control-Allow-Origin', '*' );
+
+		if ( unsupportedRoutes.includes( req.url ) ) {
+			res.end();
+		} else {
+			dbConnect()
+				.then( () => {
+					return parseRoute( req.url );
+				} )
+				.then( ( routeBits ) => {
+					let [ provider, username ] = routeBits;
+
+					if ( provider && username ) {
+						return getAccessToken( provider, username );
+					} else {
+						throw new Error( 'Did not receive either: provider; username' );
+					}
+				} )
+				.then( ( data ) => {
+					provider = ( data && data.provider && typeof data.provider === 'string' ) ? data.provider.toLowerCase() : null;
+
+					switch ( provider ) {
+						case 'instagram':
+							return fetchInstagramData( data.access_token );
+						default:
+							throw new Error( 'Failed to match provider.' );
+					}
+				} )
+				.then( ( data ) => {
+					res.end( data );
+				} )
+				.catch( ( err ) => {
+					res.end( err instanceof Error ? err.message : err );
+				} );
+		}
+	} ).listen( PORT, () => {
+		console.log( `LISTENING ON PORT: ${PORT}` );
+	} )
